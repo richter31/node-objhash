9ecd18982132e25c626698d68f52a55bcaeb1673
Nick Richter <nick@brandingbrand.com>
2021-12-10 17:14:40 -0500

    feat: node 16 support
    https://brandingbrand.atlassian.net/browse/VITACOST-700
    https://brandingbrand.atlassian.net/browse/USH-726
    
    **Description**
    * we needed to update our API projects to be on node 16 due to security issues
    * reverse engineered the fixes needed with https://github.com/nodejs/node-addon-examples
    * Tested both projects with this update and they both worked via npm link
    * once this merged, we will need to tag it and update bb-base-parsers to use it, there are also a
    few fixes needed in that repo
    
    **Test**
    * with node 16.13.1
    * npm install
    
    **ER**
    * no errors

--- src/objhash.cc
+++ src/objhash.cc
@@ -4,17 +4,27 @@ void Method(const Nan::FunctionCallbackInfo<v8::Value>& info) {
   if (info.Length() < 1) {
     info.GetReturnValue().Set(Nan::Undefined());
   } else if (info[0]->IsObject()) {
-    int hash = info[0]->ToObject()->GetIdentityHash();
+    v8::Local<v8::Context> context = info.GetIsolate()->GetCurrentContext();
+    v8::Local<v8::Object> obj = Nan::New<v8::Object>();
+    obj->Set(
+      context,
+      Nan::New("msg").ToLocalChecked(),
+      info[0]->ToObject(context).ToLocalChecked()
+    );
+    int hash = obj->GetIdentityHash();
+
     info.GetReturnValue().Set(Nan::New(hash));
   } else {
     info.GetReturnValue().Set(info[0]);
   }
 }
 
-void Init(v8::Handle<v8::Object> exports) {
+void Init(v8::Local<v8::Object> exports) {
+  v8::Local<v8::Context> context = exports->CreationContext();
   exports->Set(
+    context,
     Nan::New("ident").ToLocalChecked(),
-    Nan::New<v8::FunctionTemplate>(Method)->GetFunction()
+    Nan::New<v8::FunctionTemplate>(Method)->GetFunction(context).ToLocalChecked()
   );
 }
 
